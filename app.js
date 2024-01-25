b416e6816292c38e2d3f292db8c73fca37de6c80
MusadiqueMuslim1997 <137433475+MusadiqueMuslim1997@users.noreply.github.com>
2024-01-25 11:11:37 +0500

    add

--- app.js
+++ app.js
@@ -1,17 +1,59 @@
 
 
-function setBackground(){
+function setBackground() {
 
     var postBackground = document.getElementById("post-background");
     postBackground.style.backgroundImage = 'url(' + event.target.src + ')'
 }
-function changeBackgroundColor(){
+function changeBackgroundColor() {
     var postBackgroundColor = document.getElementById("post-background");
     console.log(event.target.value)
     postBackgroundColor.style.backgroundColor = event.target.value;
 }
 
-function changeColor(){
+function changeColor() {
     var textArea = document.getElementById("text-area");
-     textArea.style.color = event.target.value;
+    textArea.style.color = event.target.value;
+}
+function changeStyle(){
+   
+    var textArea = document.getElementById("text-area");
+    var type  = event.target.innerHTML;
+    if(type === "B"){
+        if( textArea.style.fontWeight === "bold") {
+            textArea.style.fontWeight = "normal"
+            event.target.style.fontWeight = "normal"
+        }
+        else{
+   
+        textArea.style.fontWeight = "bold";
+        event.target.style.fontWeight = "bolder"
+        }
+    }
+
+    if(type === "I"){
+        if( textArea.style.fontStyle === "italic") {
+            textArea.style.fontStyle = "normal"
+            event.target.style.fontWeight = "normal"
+        }
+        else{
+   
+        textArea.style.fontStyle = "italic";
+        event.target.style.fontWeight = "bolder"
+        
+        }
+    }
+
+    if(type === "U"){
+        if( textArea.style.textDecoration === "underline") {
+            textArea.style.textDecoration = "none"
+            event.target.style.fontWeight = "normal"
+        }
+        else{
+   
+        textArea.style.textDecoration = "underline";
+        event.target.style.fontWeight = "bolder"
+        
+        }
+    }
 }
